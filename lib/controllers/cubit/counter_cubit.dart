19998db8d4ec1b5d5da5e0c17684f9d0a8a8dc6f
Fawzi Mohammmed <fawmoh2014@gmail.com>
2026-02-09 17:35:45 +0200

    use the emit

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -5,4 +5,12 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterInit());
+
+  void increment() {
+    emit(CounterUpdate(counter: state.counter + 1));
+  }
+
+  void decrement() {
+    emit(CounterUpdate(counter: state.counter - 1));
+  }
 }
